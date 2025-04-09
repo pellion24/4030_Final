cf9c59e17bb0ff8652a01886e3039ee4c0066002
pellion24 <ashvijhosdurg@gmail.com>
2025-04-09 12:20:12 -0400

    Started work on sidemenu

---
+++ 4030_Final/SideMenu.swift
@@ -0,0 +1,18 @@
+//
+//  SideMenu.swift
+//  4030_Final
+//
+//  Created by Ashvij Hosdurg on 4/9/25.
+//
+
+import SwiftUI
+
+struct SideMenu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SideMenu()
+}
---
+++ 4030_Final/SideMenuHeader.swift
@@ -0,0 +1,18 @@
+//
+//  SideMenuHeader.swift
+//  4030_Final
+//
+//  Created by Ashvij Hosdurg on 4/9/25.
+//
+
+import SwiftUI
+
+struct SideMenuHeader: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SideMenuHeader()
+}
